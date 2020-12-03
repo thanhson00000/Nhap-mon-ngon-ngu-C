da57d224cf5a3de86740d9b3a3272c2decad145c
thanhson00000 <75401742+thanhson00000@users.noreply.github.com>
2020-12-03 10:41:02 +0700

    Create khoiluong.c

---
+++ Bài tập 2/khoiluong.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  int m ;
+  scanf("%d",&m);
+  printf("m = %d G\n ",m * 1000);
+  printf ("m | G\n");
+  printf ("%d",m);
+  printf ("   |%d",m * 1000);
+  return 0;
+}
