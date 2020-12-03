2307519360e43b8a6d50231ed8c4eaea70e920c8
thanhson00000 <75401742+thanhson00000@users.noreply.github.com>
2020-12-03 08:37:20 +0700

    Create hinhtron.C

---
+++ Bài tập 1/hinhtron.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  // Khai bao cac bien DT,CV,R Kieu double
+  printf("Hello World\n");
+  // gan gia tri R
+  return 15;
+  // Khai bao hang pi kieu double
+  const double PT = 3.14 ;
+  // Tinh chu vi 
+  CV = 2 * R * PI ;
+  // Tinh dien tich 
+  DT = R * R * PI ;
+  //  Hien thi CV , R , DT theo 2 hang 3 cot 
+  printf ("CV = 2 * R * PI=%f/n ",CV);
+  printf (" DT = R * R * PI=%f/n ",DT) ;
+  return 0 ;
+}
