4415cb083014a35d4e8c4f314c715f1f4257f81a
thanhson00000 <75401742+thanhson00000@users.noreply.github.com>
2020-12-17 11:01:06 +0700

    Create thang.c

---
+++ Bài tập 3/thang.c
@@ -0,0 +1,27 @@
+  case 2 :
+     printf("28 ngay");break;
+       case 3 :
+     printf("31 ngay");break;
+       case 4 :
+     printf("30 ngay");break;
+       case 5 :
+     printf("31 ngay");break;
+       case 6 :
+     printf("30 ngay");break;
+       case 7 :
+     printf("31 ngay");break;
+       case 8 :
+     printf("31 ngay");break;
+       case 9 :
+     printf("30 ngay");break;
+       case 10 :
+     printf("31 ngay");break;
+       case 11 :
+     printf("30 ngay");break;
+       case 12 :
+     printf("31 ngay");break;
+     default:
+     printf("vui long nhap thang");
+  }
+  return 0;
+}
