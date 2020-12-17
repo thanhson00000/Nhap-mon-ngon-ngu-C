e9a4cdeb3018bd2079de715a0195b76d0a05d687
thanhson00000 <75401742+thanhson00000@users.noreply.github.com>
2020-12-17 09:00:26 +0700

    Update hoadon.c

--- Bài tập 3/hoadon.c
+++ Bài tập 3/hoadon.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 
 int main(void) {
-  // khai bao gia tri//
-  int a, b;
-  // khai bao don gia//
-  printf("Nhap don gia hang hoa: ");
-  scanf("%d",&a);
-  // khai bao so luong//
-  printf("Nhap so luong hang hoa: ");
-  scanf("%d",&b);
-  int dongia, giatri, thanhtien;
-  // cong thuc don gia * so luong = thanh tien//
-  thanhtien = a * b;
-  printf("thanh tien "),
-  printf("=%d",thanhtien);
-  if b > 5{
-    printf("thanh tien--25%");
-  }
+			// khai bao bien dongia, so luong va thanh tien
+int dongia, soluong, thanhtien;
+			// nhap so tu ban phim
+  		printf("nhap don gia :"); 
+	scanf("%d", &dongia);
+
+		printf("nhap so luong : ");
+	scanf("%d", &soluong);
+
+			// cong thuc tinh thanh tien
+thanhtien = dongia * soluong;
+
+			// so luong neu mua >=5 va dongia>= 500 se giam 50%
+	if (soluong >= 5 , dongia >= 500){
+  		printf("so tien can phai thanh toan la %d=/2", thanhtien );
+
+}else{
+
+  		printf("so tien can phai thanh toan la %d", thanhtien);
+			// phai thoa man 2 dieu kien thi moi giam
+}
   return 0;
 }
