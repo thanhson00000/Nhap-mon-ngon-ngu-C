340a22bdd60a263e08da7fae0dcf5fc0772626fd
thanhson00000 <75401742+thanhson00000@users.noreply.github.com>
2020-12-17 08:32:16 +0700

    Create hoadon.c

---
+++ Bài tập 3/hoadon.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+  // khai bao gia tri//
+  int a, b;
+  // khai bao don gia//
+  printf("Nhap don gia hang hoa: ");
+  scanf("%d",&a);
+  // khai bao so luong//
+  printf("Nhap so luong hang hoa: ");
+  scanf("%d",&b);
+  int dongia, giatri, thanhtien;
+  // cong thuc don gia * so luong = thanh tien//
+  thanhtien = a * b;
+  printf("thanh tien "),
+  printf("=%d",thanhtien);
+  if b > 5{
+    printf("thanh tien--25%");
+  }
+  return 0;
+}
