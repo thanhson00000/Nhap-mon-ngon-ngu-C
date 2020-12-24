a4805cd2ffea70522f94389dbe6b743868173f90
thanhson00000 <75401742+thanhson00000@users.noreply.github.com>
2020-12-24 08:48:20 +0700

    Create xinchao.c

---
+++ Bài tập 3/xinchao.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int count ;
+      printf ("\t hello \n ");
+  for ( count =1 ; count <= 10 ; count ++ )
+         printf ("\n \t \t hello ");
+      printf (" \n \t \t hello. \n");
+  return 0;
+}
